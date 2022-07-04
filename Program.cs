bb2c126d346a4dbc85d2f91f4cdedba60845cc38
Irada Kh. Feyzullayeva <iradakhf@code.local>
2022-07-04 17:52:40 +0400

    file added

---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number = 0;
+            //IsOddOrEven(number);
+
+            //IsFaktorial(5);
+            //Perimetr(4, 5, 6);
+            //Console.WriteLine(Perimetr(4,5,6));
+            //Console.WriteLine(Multiplication(4, 5));
+            //Console.WriteLine(Square(4));
+            //int[] numbers = { 12, 3, 5, 87 };
+            //SumOfArray(numbers);
+            //Console.WriteLine(Welcome("Irada", "Feyzullayeva"));
+            //Console.WriteLine(Reverse("Irada"));
+            //Console.WriteLine(Empty("hello my friend"));
+        }
+        //static void IsOddOrEven(int number)
+        //{
+        //    if (number == 0)
+        //    {
+        //        Console.WriteLine("{0} is zero", number);
+        //    }
+        //    else if (number%2 == 0)
+        //    {
+        //        Console.WriteLine("is even");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("{0} is odd",number);
+        //    }
+        //}
+
+        //static void IsFaktorial(int number)
+        //{ int faktorial = 1;
+        //    if(number==0)
+        //    { 
+        //        Console.WriteLine("it is zero");
+        //    }
+        //    int a = 0;
+        //    while (faktorial < number)
+        //    {
+        //        a = number * faktorial;
+
+        //        faktorial++;
+        //    }
+        //    Console.WriteLine(a);
+        //}
+
+        //static int Perimetr(int a)
+        //{
+        //    return 4 * a;
+        //}
+        //static int Perimetr(int a, int b, int c)
+        //{
+        //    return a + b + c;
+        //}
+
+        //gonderilmisikiinthasilinintapanmethod
+
+        //static int Multiplication(int a, int b)
+        //{
+        //    return a * b;
+        //}
+
+        //static int Square(int a)
+        //{
+        //    return a*a;
+        //}
+
+        //static void SumOfArray (int[] numbers)
+        //{
+        //    int sum = 0;
+        //    for (int i = 0; i < numbers.Length; i++)
+        //    {
+        //        sum += numbers[i];
+
+        //    }
+        //    Console.WriteLine(sum);
+
+        //}
+        //static void Welcome(string name, string surname)
+        //{
+        //    Console.WriteLine($"welcome,{name},{surname}")
+        //}
+
+        //static string Reverse(string name)
+        //{
+        //    string reverseWord="";
+        //    for (int i=name.Length-1; i >= 0; i--)
+        //    {
+        //        reverseWord += name[i];        
+        //    }
+        //    return reverseWord;
+        //}
+
+        //static int Empty(string sentence)
+        //{
+        //    int count = 0;
+        //    for (int i = 0; i < sentence.Length; i++)
+        //    {
+        //        if (sentence[i] == ' ')
+        //        {
+        //            count++;
+                    
+        //        }
+
+        //    }
+        //    return count;
+        //}
+    }
+}
+
